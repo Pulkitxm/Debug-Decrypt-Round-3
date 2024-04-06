11deaaeedee04a8ae506d26592ddca7a21d3707e
Pulkit <kpulkit15234@gmail.com>
2024-04-06 11:13:05 +0530

    update q6

--- encoder6/main.cpp
+++ encoder6/main.cpp
@@ -24,33 +24,11 @@ std::string encoder(const std::string& str, const std::string& key) {
     return encoded_str;
 }
 
-std::string decoder(const std::string& str, const std::string& key) {
-    std::string decoded_str;
-    std::vector<std::vector<char>> table = generateCipherTable();
-    for (size_t i = 0; i < str.length(); i++) {
-        if (isalpha(str[i])) {
-            char c = toupper(str[i]);
-            char k = toupper(key[i % key.length()]);
-            for (int j = 0; j < 26; j++) {
-                if (table[k - 65][j] == c) {
-                    decoded_str += static_cast<char>(j + 65);
-                    break;
-                }
-            }
-        } else {
-            decoded_str += str[i];
-        }
-    }
-    return decoded_str;
-}
-
 int main() {
     std::string string, key;
     std::getline(std::cin, string);
     std::getline(std::cin, key);
     std::string encoded_str = encoder(string, key);
     std::cout << "Encoded string: " << encoded_str << std::endl;
-    std::string decoded_str = decoder(encoded_str, key);
-    std::cout << "Decoded string: " << decoded_str << std::endl;
     return 0;
 }
