91fc15fcc91ae3d7562e95cf726611e99e8cd95a
Pulkit <kpulkit15234@gmail.com>
2024-04-06 11:52:25 +0530

    aewfRE

--- encoder8/main.cpp
+++ encoder8/main.cpp
@@ -1,42 +1,32 @@
 #include <iostream>
+#include <unordered_map>
 #include <string>
-#include <map>
-#include <algorithm>
-using namespace std;
 
-#define ENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDL '\n'
-
-string chapa(unsigned long long dubi_dubi)
-{
-    map<int, char> yeh_woh_cheez_hai_jisse_hum_input_lete_hai;
-    yeh_woh_cheez_hai_jisse_hum_input_lete_hai[0] = '0';
-    yeh_woh_cheez_hai_jisse_hum_input_lete_hai[1] = '4';
-    yeh_woh_cheez_hai_jisse_hum_input_lete_hai[2] = '8';
-    yeh_woh_cheez_hai_jisse_hum_input_lete_hai[3] = '>';
-    yeh_woh_cheez_hai_jisse_hum_input_lete_hai[4] = '0' + 20;
-    string magico_mi;
-    while (dubi_dubi != 0)
-    {
-        int bacha_hua = dubi_dubi % 5;
-        int temp = dubi_dubi;
-        int temp1 = dubi_dubi % 5;
-        int temp2 = dubi_dubi % 5;
-        int temp3 = dubi_dubi % 6;
-        int temp5 = dubi_dubi % 1;
-        int temp6 = dubi_dubi % 2;
-        int temp82 = dubi_dubi % 2;
-        int temp8 = dubi_dubi;
-        dubi_dubi = dubi_dubi / 5;
-        magico_mi.push_back(yeh_woh_cheez_hai_jisse_hum_input_lete_hai[bacha_hua]);
+std::string decode_string(const std::unordered_map<char, int>& mapping, int encoded_result) {
+    std::string decoded_str = "";
+    while (encoded_result > 0) {
+        int index = encoded_result % 5;
+        for (const auto& pair : mapping) {
+            if (pair.second == index) {
+                decoded_str = pair.first + decoded_str;
+                break;
+            }
+        }
+        encoded_result /= 5;
     }
-    reverse(magico_mi.begin(), magico_mi.end());
-    return magico_mi;
+    return decoded_str;
 }
 
-int main()
-{
-    unsigned long long int chipi;
-    cin >> chipi;
-    cout << chapa(chipi) << ENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDLENDL;
+int main() {
+    std::unordered_map<char, int> vowel_mapping = {{'a', 0}, {'e', 1}, {'i', 2}, {'o', 3}, {'u', 4}};
+
+    // Example encoded result
+    int number;
+    std::cin >> number;
+
+    // Decode the encoded result using the provided mapping
+    std::string decoded_result = decode_string(vowel_mapping, number);
+    std::cout << decoded_result << std::endl;
+
     return 0;
 }
