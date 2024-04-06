2672e1fa0648d57029f71d1f32da427df0968f04
Pulkit <kpulkit15234@gmail.com>
2024-04-06 11:36:49 +0530

    AEFaf

--- encoder4/main.py
+++ encoder4/main.py
@@ -1,36 +1,28 @@
-def fibonacci_sequence(n):
-    sequence = [0, 1]
-    while len(sequence) < n:
-        sequence.append(sequence[-1] + sequence[-2])
-    return sequence
+def prime_factors(n):
+    factors = []
+    divisor = 2
 
-def fibonacci_encoder(text):
-    encoded_text = ''
-    fibonacci_numbers = fibonacci_sequence(len(text))
-    for i, char in enumerate(text):
-        encoded_char = chr((ord(char) + fibonacci_numbers[i]) % 128)
-        encoded_text += encoded_char
-    return encoded_text
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
 
-def fibonacci_decoder(encoded_text):
-    decoded_text = ''
-    fibonacci_numbers = fibonacci_sequence(len(encoded_text))
-    for i, char in enumerate(encoded_text):
-        decoded_char = chr((ord(char) - fibonacci_numbers[i]) % 128)
-        decoded_text += decoded_char
-    return decoded_text
+    return factors
+    
 
-def main():
-    text = input("Enter the text: ")
-    choice = input("Enter 'e' for encoding or 'd' for decoding: ")
-    if choice == 'e':
-        encoded_text = fibonacci_encoder(text)
-        print("Encoded text:", encoded_text)
-    elif choice == 'd':
-        decoded_text = fibonacci_decoder(text)
-        print("Decoded text:", decoded_text)
+def printarr(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+s = input()[::-1]
+ans=[]
+space = [2, 2, 2, 2, 2]
+for c in s:
+    if (c==" "):
+        ans.extend(space)
     else:
-        print("Invalid choice!")
-
-if __name__ == "__main__":
-    main()
+        ans.extend(prime_factors(ord(c)))
+print(len(ans))
+printarr(ans)
