27f4855fd739b45fcaf37b862d4e38e277177e28
Pulkit <kpulkit15234@gmail.com>
2024-04-06 10:49:47 +0530

    added

--- encoder2/Main.java
+++ encoder2/Main.java
@@ -1,59 +1,70 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int[][] _1(int[][] matrix) {
-        int[][] _2 = new int[matrix.length][matrix[0].length];
-        for (int _3 = 0; _3 < matrix.length; _3++) {
-            for (int _4 = matrix[0].length - 1; _4 >= 0; _4--) {
-                _2[_3][matrix[0].length - 1 - _4] = matrix[_3][_4];
+    public static int[][] kadshfuahfiejfopWKFPOfk(int[][] matrix) {
+        int[][] mirroredMatrix = new int[matrix.length][matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = matrix[0].length - 1; j >= 0; j--) {
+                mirroredMatrix[i][matrix[0].length - 1 - j] = matrix[i][j];
             }
         }
-        return _2;
+
+        return mirroredMatrix;
     }
 
-    public static int[][] _2(int[][] matrix) {
-        int[][] _3 = new int[matrix.length][matrix[0].length];
-        for (int _4 = matrix.length - 1; _4 >= 0; _4--) {
-            _3[matrix.length - 1 - _4] = matrix[_4];
+    public static int[][] whgfuiasdhfuasjfijs(int[][] matrix) {
+        int[][] whgfuiasdhfuasjfijsMatrix = new int[matrix.length][matrix[0].length];
+
+        for (int i = matrix.length - 1; i >= 0; i--) {
+            whgfuiasdhfuasjfijsMatrix[matrix.length - 1 - i] = matrix[i];
         }
-        return _3;
+
+        return whgfuiasdhfuasjfijsMatrix;
     }
 
-    public static int[][] _3(int[][] matrix) {
-        int[][] _4 = new int[matrix.length][matrix[0].length];
-        for (int _5 = 0; _5 < matrix.length; _5++) {
-            for (int _6 = 0; _6 < matrix[0].length; _6++) {
-                _4[_5][_6] = matrix[_5][_6] + 5;
+    public static int[][] jshdgfjasgfuase(int[][] matrix) {
+        int[][] updatedMatrix = new int[matrix.length][matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                updatedMatrix[i][j] = matrix[i][j] + 5;
             }
         }
-        return _4;
+
+        return updatedMatrix;
     }
 
-    public static void _4(int[][] matrix) {
-        for (int[] _5 : matrix) {
-            for (int _6 : _5) {
-                System.out.print(_6 + " ");
+    public static void ahgdvhjabf(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        Scanner _5 = new Scanner(System.in);
-        int _6 = _5.nextInt();
-        int _7 = _5.nextInt();
-        int[][] _8 = new int[_6][_7];
-        for (int _9 = 0; _9 < _6; _9++) {
-            for (int _10 = 0; _10 < _7; _10++) {
-                _8[_9][_10] = _5.nextInt();
+        Scanner scanner = new Scanner(System.in);
+
+        int r = scanner.nextInt();
+        int c = scanner.nextInt();
+        int[][] matrix = new int[r][c];
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = scanner.nextInt();
             }
         }
-        System.out.println(_6);
-        System.out.println(_7);
-        int[][] _9 = _3(_8);
-        int[][] _10 = _2(_9);
-       // int[][] _11 = _3(_10);
-        _4(_10);
-        _5.close();
+
+        System.out.println(r);
+        System.out.println(c);
+        
+        int[][] mirror = kadshfuahfiejfopWKFPOfk(matrix);
+        int[][] water = whgfuiasdhfuasjfijs(mirror);
+        int[][] finalMatrix = jshdgfjasgfuase(water);
+        ahgdvhjabf(finalMatrix);
+
+        scanner.close();
     }
 }
